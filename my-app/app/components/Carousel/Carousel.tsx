578b4da8f08e431c057d3ff2c8ee2a6df362cf31
milosmladenovic89 <milos.mladenovic@foreach.rs>
2024-03-11 14:31:14 +0100

    progress

--- my-app/app/components/Carousel/Carousel.tsx
+++ my-app/app/components/Carousel/Carousel.tsx
@@ -1,10 +1,38 @@
-
+import EmblaCarousel from './EmblaCarousel'
+import { EmblaOptionsType } from 'embla-carousel'
 
 export default function Carousel() {
-    return (
-        <div className="w-full lg:w-800p mx-auto mt-80 text-center">
 
-            <div className="mx-auto carousel carousel-center w-full h-96 p-4 space-x-4 bg-neutral rounded-box  shadow-2xl carousel-class">
+
+
+    const OPTIONS: EmblaOptionsType = { loop: true }
+    const SLIDE_COUNT = 5
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+
+
+
+    return (
+        <div className="w-full lg:w-800p mx-auto mt-80 ">
+
+
+            <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <div className="mx-auto carousel carousel-center w-full h-96 p-4 space-x-4 bg-neutral rounded-box  shadow-2xl carousel-class">
 
                 <div id="item1" className="carousel-item w-3/4">
                     <div className="border border-gray-700 rounded-2xl    h-full box-grid-div w-full"></div>
@@ -27,6 +55,8 @@ export default function Carousel() {
                 <a href="#item3" className="btn rounded-xl h-2  w-6  btn-xs"></a>
                 <a href="#item4" className="btn rounded-xl h-2  w-6  btn-xs"></a>
             </div>
+    */}
+
         </div>
     )
 }
